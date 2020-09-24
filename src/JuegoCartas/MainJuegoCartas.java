825d73929a6ce421b24fcc7f015e3569ae9e2368
Braian Leonel Vogrič <53317134+Vogric@users.noreply.github.com>
2020-09-24 17:45:55 -0300

    Test push/pull

--- src/JuegoCartas/MainJuegoCartas.java
+++ src/JuegoCartas/MainJuegoCartas.java
@@ -5,6 +5,8 @@ public class MainJuegoCartas {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		// Test push/pull
+		Juego juego1 = new Juego(0, null, null, null);
 	}
 
 }
