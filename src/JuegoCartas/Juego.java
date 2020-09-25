076d7d4478e295810e8feed99e11d4c8c05202f2
Braian Leonel Vogrič <53317134+Vogric@users.noreply.github.com>
2020-09-25 10:11:51 -0300

    Test push/pull

--- src/JuegoCartas/Juego.java
+++ src/JuegoCartas/Juego.java
@@ -5,7 +5,7 @@ public class Juego {
 	private MazoCartas mazo;
 	private Jugador jugadorA;
 	private Jugador jugadorB;
-	
+
 	public Juego(int maxRondas, MazoCartas mazo, Jugador jugadorA, Jugador jugadorB) {
 		this.maxRondas = maxRondas;
 		this.mazo = mazo;
@@ -15,46 +15,48 @@ public class Juego {
 
 	public void asignarTurno() {
 	}
-	
-	public boolean esGanador() {	
+
+	public boolean esGanador() {
 		return false;
 	}
-	
-	public void rondaJuego() {
-		int ronda = 0;
-		while (ronda < maxRondas) {
+
+	public void jugar() {
+		for (int ronda = 0; ronda < this.maxRondas; ronda++) {
 			this.jugadorA.jugar();
 			this.jugadorB.jugar();
 			this.finDeJuego(ronda);
-			ronda++;
 		}
 	}
-	
+
 	public boolean finDeJuego(int ronda) {
-		return (this.getMaxRondas() == ronda || 
-				this.jugadorA.contarCartas() == 0 || 
-				this.jugadorB.contarCartas() == 0);
+		return (this.getMaxRondas() == ronda || this.jugadorA.contarCartas() == 0 || this.jugadorB.contarCartas() == 0);
 	}
-	
+
+	@Override
+	public String toString() {
+		return "JUEGO" + "\nMáximo de rondas: " + this.getMaxRondas() + "\nJugador A: " + this.getJugadorA().getNombre()
+				+ "\nJugador B: " + this.getJugadorB().getNombre();
+	}
+
 // GETTERS & SETTERS
 	public int getMaxRondas() {
 		return maxRondas;
 	}
-	
+
 	public void setMaxRondas(int maxRondas) {
 		this.maxRondas = maxRondas;
 	}
-	
+
 	public MazoCartas getMazo() {
 		return mazo;
 	}
-	
+
 	public Jugador getJugadorA() {
 		return jugadorA;
 	}
-	
+
 	public Jugador getJugadorB() {
 		return jugadorB;
 	}
-	
+
 }
--- src/JuegoCartas/MainJuegoCartas.java
+++ src/JuegoCartas/MainJuegoCartas.java
@@ -5,8 +5,14 @@ public class MainJuegoCartas {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		// Test push/pull
-		Juego juego1 = new Juego(0, null, null, null);
+		// INSTANCIAS y prueba
+		Jugador jugadorA = new Jugador("Federico");
+		Jugador jugadorB = new Jugador("Josué");
+		MazoCartas mazo = new MazoCartas(50);
+		Juego juego = new Juego(40, mazo, jugadorA, jugadorB);
+
+		// IMPRIME POR CONSOLA
+		System.out.println(juego);
 	}
 
 }
