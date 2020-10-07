d253ff64a68e5f7b04d6f77937601f1c55f7392f
Seba <sebastianbellidodg@gmail.com>
2020-10-07 12:25:46 -0300

    main mejorado, falta imprimir la vitacora del juego

--- src/JuegoCartas/MainJuegoCartas.java
+++ src/JuegoCartas/MainJuegoCartas.java
@@ -6,6 +6,11 @@ public class MainJuegoCartas {
 
         String mazoPath = "./superheroes.json";
         VisorMazo.mostrarMazo(mazoPath);
+        MazoCartas mazo = VisorMazo.cargarMazo(mazoPath);
+        Jugador jugadorA = new Jugador("jugadorA");
+        Jugador jugadorB = new Jugador("jugadorB");
+        Juego juego = new Juego(10, mazo, jugadorA, jugadorB);
+        juego.jugar();
     }
 }
 
